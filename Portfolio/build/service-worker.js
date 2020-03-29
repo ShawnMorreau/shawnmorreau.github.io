da72aead2a2f68b12d8c7ccdff1c9847e0587f41
shawnmorreau <shawn.morreau@hotmail.com>
2020-03-29 05:17:13 -0400

    fix

--- Portfolio/build/service-worker.js
+++ Portfolio/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.65a71e91b8b62f20724e607e661706b1.js"
+  "/precache-manifest.ab608716f52696292c7fd5a0c9354f36.js"
 );
 
 self.addEventListener('message', (event) => {
