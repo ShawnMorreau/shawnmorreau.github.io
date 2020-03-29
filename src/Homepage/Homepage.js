adec8f70cf06e4b7cca3637f8b64f906b6e583d7
shawnmorreau <shawn.morreau@hotmail.com>
2020-03-29 03:52:37 -0400

    move out of folder

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -4,6 +4,7 @@ export default function Homepage(){
     return(
         <div className="main">
             <h1>Welcome to the underground</h1>
+            <p> rrebuild</p>
         </div>
     );
 }
