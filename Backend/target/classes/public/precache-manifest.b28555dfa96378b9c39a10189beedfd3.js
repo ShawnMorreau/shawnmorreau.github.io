5ea402c0724e007e20e2a61d614c93600a95aae4
shawnmorreau <shawn.morreau@hotmail.com>
2020-04-28 23:50:26 -0400

    do it

---
+++ Backend/target/classes/public/precache-manifest.b28555dfa96378b9c39a10189beedfd3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc763072f598b39774b3e8e66b5e6d7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18f048f6847169a43aa1",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "2ed94382f40d6dbf9dc5",
+    "url": "/static/css/main.63b48de5.chunk.css"
+  },
+  {
+    "revision": "18f048f6847169a43aa1",
+    "url": "/static/js/2.05ddfba5.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.05ddfba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ed94382f40d6dbf9dc5",
+    "url": "/static/js/main.455efc70.chunk.js"
+  },
+  {
+    "revision": "a27870e5f03a3425f6aa",
+    "url": "/static/js/runtime-main.c70048c8.js"
+  },
+  {
+    "revision": "6ef615677e917a9f4db581cc0898bed4",
+    "url": "/static/media/bg.6ef61567.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "847abb3796cbe32e7f6f69932cf7eaf7",
+    "url": "/static/media/me.847abb37.jpg"
+  }
+]);
