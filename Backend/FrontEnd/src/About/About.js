c3c96a489a8db5a27a940c8dfc0df745b0d46531
ShawnMorreau <Shawn.morreau@hotmail.com>
2020-09-27 20:41:23 -0400

    copy of old website

--- Backend/FrontEnd/src/About/About.js
+++ Backend/FrontEnd/src/About/About.js
@@ -1,29 +1,36 @@
 import React from "react";
 import "./About.css";
 import Image from "../images/me.jpg";
-import CardV1 from "../Card/CardV1/CardV1.js";
+import Skill from "../Skills/Skill";
+import Typography from "@material-ui/core/Typography";
+
 export default function About() {
-    return (
-        <>
-        <div id="navBarSpacer"></div>
-        <div id="about">
-            <div className="spacer"></div>
-            <img src={Image} alt="" className="profilePic"/>
-            <h2>How you doin, I see you.</h2>
-            <p>Ma name is Shawn!!!</p>
-            <p> Hola, mi llamo Shawn. Estoy muy fuego. Queiro trabajo por favor.
-                mi esposa llamas Jaymee.
-                I need to put a whole bunch of stuff in here but I also don't need to? 
-                Should I even have this paragraph here? Do I just put stuff like my resume?
-                people don't like to read stuff anyways? put info into a chart or something?
-                education, what I enjoy to do, ez?
-            </p>
-            <hr id="spacing"/>
-           <div className="skillsCards">
-                <CardV1/>
-                
-           </div>
-        </div>
-        </>
-    );
+  return (
+    <>
+      <div id="navBarSpacer"></div>
+      <div id="about">
+        <div className="spacer"></div>
+        <img src={Image} alt="" className="profilePic" />
+        <Typography variant = "h3" component="h1">Hello. My name is Shawn!</Typography>
+        <Typography id="description" component = "p">
+          Thank you for taking interest in me. I have a Bachelor's of
+          Engineering with focus in Software Engineering. I owe my foundations
+          in backend development to school, but through self learning I have
+          also picked up enough knowledge to create sleek front-end experiences.
+          I am always eager to continue learning through building and
+          collaborating with others!
+        </Typography>
+        <hr id="spacing" />
+        <section className="skills">
+          <Typography variant="h3" component="h3">
+            Skills/Knowledge
+          </Typography>
+          <Skill 
+            title="tonka" 
+
+            />
+        </section>
+      </div>
+    </>
+  );
 }
--- Backend/FrontEnd/src/Skills/Skill.js
+++ Backend/FrontEnd/src/Skills/Skill.js
@@ -1,118 +1,12 @@
 import React from "react";
-import "./SkillsContainer.css";
-import { makeStyles } from '@material-ui/core/styles';
-
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import CardHeader from "@material-ui/core/CardHeader";
-import Avatar from "@material-ui/core/Avatar";
-
-import Typography from "@material-ui/core/Typography"
-import Paper from "@material-ui/core/Paper";
-import Chip from "@material-ui/core/Chip";
-
-import PropTypes from 'prop-types';
-
-
-const useStyles = makeStyles(theme => ({
-    root: {
-        minWidth: 350,
-        height:200,
-        backgroundColor: 'blue',
-        margin: '0 0 5px 0'
-    },
-    skillContainer: {
-        display: 'flex',
-        flexDirection: 'column',
-        flex: 1,
-        listStyle: 'none',
-        padding: 0,
-        margin: 0,
-        backgroundColor: 'gray',
-        minWidth:130,
-        height:120
-    },
-    skill: {
-        margin: theme.spacing(0.5),
-        border: '1px solid rgba(0,0,0,0.5)',
-        fontSize: "0.9rem",
-    },
-    skillDef:{
-        height:120,
-        width:'100%',
-        margin:0,
-        padding:5,
-        textAlign:"center"
-    },
-    title: {
-        fontSize: '1rem',
-        color: '#ff0000',
-        margin: 0,
-    },
-    body: {
-        fontSize: 12,
-        margin: 0,
-    },
-    avatar: {
-        color: 'black',
-        backgroundColor: 'red',
-        margin: 0,
-    },
-    Card: {
-        margin: '0 15px',
-        padding: 0,
-        display:'flex',
-        flexWrap:'none',
-        minWidth:100,
-        height:120,
-    }
-}))
-
-export default function Skill(props) {
-    const classes = useStyles();
-    const [tab,setTab] = React.useState();
-
-    const handleClick=(e)=>{
-
-    }
-    return (
-        <Card className={classes.root}>
-            <CardHeader
-                avatar={
-                    <Avatar aria-label={props.title} className={classes.avatar}>
-                        {props.title.slice(0, 1)}
-                    </Avatar>
-                }
-                title={
-                    <Typography
-                        className={classes.title}
-                        component="h1"
-                        variant="button"
-                        align="left"
-                    >
-                        {props.title}
-                    </Typography>
-                }
-
-            />
-            <CardContent className={classes.Card}>
-                <Paper component="ul" className={classes.skillContainer} align="center">
-                    {props.skills.map(data => {
-                        return (
-                            <li key={data.key}>
-                                <Chip
-                                    label={data.value}
-                                    className={classes.skill}
-                                    onClick={handleClick}
-                                />
-                            </li>
-                        );
-                    })}
-                </Paper>
-                <Paper component="p" className={classes.skillDef}>
-                    {"Click on a skill to expand"}
-                </Paper>
-            </CardContent>
-        </Card>
-    );
-}
+import PropTypes from "prop-types";
+const Skill = (props) => {
+    const {title} = props;
+    return(
+        <section className = "skill">
+            
+        </section>
+        
+    )
+};
+export default Skill;
