735be44643f0de13f08f541c08e4b58159615bc0
ShawnMorreau <Shawn.morreau@hotmail.com>
2020-11-04 02:44:52 -0500

    cleanup

---
+++ FrontEnd/build/precache-manifest.42f60bb9667f73fa929fdebfbcd922b3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab6ba47f96228e6e60aff8f3881a8be9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf823171debd3bd0a4fb",
+    "url": "/static/css/main.95af18a4.chunk.css"
+  },
+  {
+    "revision": "60c32ec41ac2ee79133f",
+    "url": "/static/js/2.3ac3edf0.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.3ac3edf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf823171debd3bd0a4fb",
+    "url": "/static/js/main.8b8d6144.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "9a6b2e8de1acffdb1246fe46ad1fa20b",
+    "url": "/static/media/backgroundv2.9a6b2e8d.jpg"
+  },
+  {
+    "revision": "65ae829ceecce7f630d554617c574de2",
+    "url": "/static/media/contact.65ae829c.png"
+  },
+  {
+    "revision": "9231c9a4c6d2821027a86e282e5d79a9",
+    "url": "/static/media/filledLogo.9231c9a4.svg"
+  },
+  {
+    "revision": "b0f6823fb3d32301c0afa856000d7fa3",
+    "url": "/static/media/logo.b0f6823f.png"
+  },
+  {
+    "revision": "f6df8c367d6c9adbc72107fab5cb8eed",
+    "url": "/static/media/me.f6df8c36.jpg"
+  },
+  {
+    "revision": "ae29f2c24f315e080ee6f476f496368a",
+    "url": "/static/media/rsz_binary.ae29f2c2.png"
+  },
+  {
+    "revision": "3e877ee2b06d29ff850249bedd01f133",
+    "url": "/static/media/rsz_projects_2.3e877ee2.png"
+  }
+]);
